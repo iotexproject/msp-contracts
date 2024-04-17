5b25571585c38cf01411ea20d3be550be860ffd4
ququzone <xueping.yang@gmail.com>
2024-04-17 22:53:15 +0800

    add deploy script

---
+++ script/Deployer.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+
+contract Deployer is Script {
+    function run() external {
+    }
+}
---
+++ src/interfaces/IBucket.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IBucket {
+    function bucketOf(uint256 bucketId)
+        external
+        view
+        returns (uint256 amount, uint256 duration, uint256 unlockedAt, uint256 unstakedAt, address delegate);
+}
