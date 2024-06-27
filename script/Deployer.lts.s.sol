3d55e89338630cd9e5eba65248e5a5197eca4160
ququzone <xueping.yang@gmail.com>
2024-06-27 16:10:59 +0800

    complete deploy scripts

---
+++ script/Deployer.lts.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import {IStrategyManager} from "src/interfaces/IStrategyManager.sol";
+import {LSTStrategy} from "src/strategies/LSTStrategy.sol";
+
+contract Deployer is Script {
+    address private proxyAdmin;
+    address private strategyManager;
+    address private lstToken;
+    uint256 private lstRatio;
+
+    function setUp() external {
+        proxyAdmin = vm.envAddress("PROXY_ADMIN_ADDRESS");
+        strategyManager = vm.envAddress("STRATEGY_MANAGER");
+        lstToken = vm.envAddress("LST_TOKEN");
+        lstRatio = vm.envUint("LST_RATIO");
+
+        console.log("PROXY_ADMIN_ADDRESS: '%s'", proxyAdmin);
+        console.log("STRATEGY_MANAGER: '%s'", strategyManager);
+        console.log("LST_TOKEN: '%s'", lstToken);
+        console.log("LST_RATIO: '%d'", lstRatio);
+    }
+
+    function run() external {
+        vm.startBroadcast();
+
+        LSTStrategy strategyImpl = new LSTStrategy();
+        TransparentUpgradeableProxy strategy = new TransparentUpgradeableProxy(
+            address(strategyImpl), proxyAdmin, abi.encodeCall(LSTStrategy.initialize, (lstToken, strategyManager))
+        );
+
+        IStrategyManager strategyManagerProxy = IStrategyManager(address(strategyManager));
+        strategyManagerProxy.addStrategy(address(strategy), lstRatio);
+
+        vm.stopBroadcast();
+
+        console.log("Deployed LSTStrategy for %s LST: '%s'", lstToken, address(strategy));
+    }
+}
--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -7,6 +7,7 @@ import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.s
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 import {BucketRewardPool} from "src/core/BucketRewardPool.sol";
+import {IStrategyManager, StrategyManager} from "src/core/StrategyManager.sol";
 import {BucketStrategy} from "src/strategies/BucketStrategy.sol";
 
 contract Deployer is Script {
@@ -26,6 +27,7 @@ contract Deployer is Script {
 
         if (proxyAdmin == address(0)) {
             proxyAdmin = address(new ProxyAdmin());
+            console.log("Deployed PROXY_ADMIN_ADDRESS: '%s'", proxyAdmin);
         }
 
         // deploy BucketRewardPool
@@ -34,17 +36,28 @@ contract Deployer is Script {
             address(bucketRewardPoolImpl), proxyAdmin, abi.encodeCall(BucketRewardPool.initialize, ())
         );
 
+        // deploy StrategyManager
+        StrategyManager strategyManagerImpl = new StrategyManager();
+        TransparentUpgradeableProxy strategyManager = new TransparentUpgradeableProxy(
+            address(strategyManagerImpl), proxyAdmin, abi.encodeCall(StrategyManager.initialize, ())
+        );
+
         // deploy BucketStrategy
         BucketStrategy strategyImpl = new BucketStrategy();
         TransparentUpgradeableProxy strategy = new TransparentUpgradeableProxy(
             address(strategyImpl),
             proxyAdmin,
-            abi.encodeCall(BucketStrategy.initialize, (bucketNFT, address(0), address(bucketRewardPool)))
+            abi.encodeCall(BucketStrategy.initialize, (bucketNFT, address(strategyManager), address(bucketRewardPool)))
         );
+
+        // will fail at IoTeX chain
+        // IStrategyManager strategyManagerProxy = IStrategyManager(address(strategyManager));
+        // strategyManagerProxy.addStrategy(address(strategy), 100);
 
         vm.stopBroadcast();
 
         console.log("Deployed BucketRewardPool: '%s'", address(bucketRewardPool));
+        console.log("Deployed StrategyManager: '%s'", address(strategyManager));
         console.log("Deployed BucketStrategy: '%s'", address(strategy));
     }
 }
